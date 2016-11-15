78f236cf7d8849ca1f31295bd0d2553dc6171d3c
Benrico42115010 <benricosimamora@gmail.com>
2016-11-15 08:37:34 +0700

    Add files via upload

---
+++ sketch_nov07b.ino
@@ -0,0 +1,18 @@
+const int switchPin = 2;
+const int motoraPin = 9;
+int switchState = 0;
+void setup() {
+
+   pinMode(motorPin, OUTPUT);
+   pinMode(switchPin, INPUT);
+}
+void loop() {
+  switchState = digitalRead(switchPin);
+  if (switchState == HIGH) {
+    digitalWrite(motoraPin, HIGH);
+  }
+  else {
+    digitalWrite(motorPin, LOW);
+  }
+}
+
